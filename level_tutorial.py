ae5dd71b4ae88e6b73b6d8e117289ed217113e1f
ams4222 <ams4222@g.rit.edu>
2022-02-26 11:28:49 -0500

    updated tutorial level

---
+++ level_tutorial.py
@@ -0,0 +1,20 @@
+import pygame, sys
+
+level_tutorial_map = [
+'                                                               ',
+'                                                               ',
+'          X                                    XXXX            ',
+'          X    XXX                               XXX           ',
+'          X                                      XXXX          ',
+'          X                                      XXXXX         ',
+'          XXXXXXXXXXXXXXXXX    XXX       B       XXXXXX        ',
+'          XXXXXXXXXXXXXXXXXXXXXXXX     XXX       XXXXXXX       ',
+'                                         X       XXXXXXXX      ',
+'                       X    X             X       XXXXXXXXX    ',
+'                                               BXXXXXXXXXXX    ',
+'         S    XXXX  X   XX    X XXXXXX           XXXXXXXXXXX   ',
+' P   XXXXXXXXXXXXX      XX      XXXXXXXX     B   XXXXXXXXXXXXX ',
+' XXXXXXXXXXXXXXXXX      XX      XXXXXXXX   XXXXXXXXXXXXXXXXXXXE',
+' XXXXXXXXXXXXXXXXX      XX      XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+' XXXXXXXXXXXXXXXXXSSSSSSXXSSSSSSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+]
