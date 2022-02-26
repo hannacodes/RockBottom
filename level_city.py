4bb8b90e70ff0007e94de68f6c54c57d67fdf0a5
ams4222 <ams4222@g.rit.edu>
2022-02-26 11:53:10 -0500

    city level initial upload

---
+++ level_city.py
@@ -0,0 +1,24 @@
+import pygame, sys
+
+level_city_map =[
+'                                                                          ',
+'                                                                          ',
+'                                                          XX      SSXX    ',
+'                                                          XX        XX    ',
+'                                                          XXSS      XX    ',
+'                                                          XX       SXX    ',
+'                                                          XXSS      XX    ',
+'                                                          XX        XX    ',
+'                                                          XX   SS   XX    ',
+'                                                          XX        XX    ',
+'                                                          XX      SSXX    ',
+'                                                          XX        XX    ',
+'                                                          XXS       XX    ',
+'                                                          XX   S   SXX    ',
+'                                                          XX        XX    ',
+'                                                          XXS       XX    ',
+'                                                          XX       SXX    ',
+'                                                          XX   SS   XX    ',
+'                                                          XX            E ',
+'                                                          XXXXXXXXXXXXXXX '
+]
