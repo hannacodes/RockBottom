504dea60016e1642be81e2fd7c8d74f664e71b09
Hanna Koh <hjk5643@g.rit.edu>
2022-02-26 13:30:03 -0500

    Added level select screen
    made main menu work with game levels

--- menus.py
+++ menus.py
@@ -1,6 +1,5 @@
-
-from cProfile import run
 import pygame as pg, pygame_gui as pgui, sys
+from main import level_1
 clock = pg.time.Clock()
 pg.init()
 info = pg.display.Info()
@@ -9,7 +8,34 @@ bg_color = (123, 123, 123)
 screen_width, screen_height = 1200, 900
 
 def level_select(screen):
-    pass
+    manager = pgui.UIManager((screen_height, screen_width), 'theme.json')
+    layout_rect_1 = pg.Rect(screen_width/3 - screen_width/6, screen_height/2 - screen_width/4, screen_width/4, screen_width/4)
+    layout_rect_2 = pg.Rect((screen_width/3)*2 - screen_width/6, screen_height/2 - screen_width/4, screen_width/4, screen_width/4)
+
+    level1 = pgui.elements.UIButton(relative_rect=layout_rect_1, text='Level One', manager=manager)
+    level2 = pgui.elements.UIButton(relative_rect=layout_rect_2, text='Level Two', manager=manager)
+
+    running = True 
+    while running: 
+        screen.fill(bg_color)
+        manager.update(60/1000.0)
+        manager.draw_ui(screen)
+        pg.display.update()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    running = False
+            if event.type == pgui.UI_BUTTON_PRESSED:
+                if event.ui_element == level1:
+                    level_1()
+                if event.ui_element == level2:
+                    pass
+            manager.process_events(event)
+        clock.tick(60)
+
 
 def help(screen):
     manager = pgui.UIManager((screen_height, screen_width))
