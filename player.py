188d9006eab3c24486eca3ea1f33028e61ca1afc
ams4222 <ams4222@g.rit.edu>
2022-02-26 23:43:54 -0500

    fixed sizing and added animations

--- player.py
+++ player.py
@@ -5,35 +5,36 @@ class Player(pg.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.sprites = []
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(1).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(2).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(3).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(4).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(5).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(6).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(7).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(8).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(9).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(10).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(11).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(12).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(13).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(14).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(15).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(16).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(17).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(18).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(19).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(20).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(21).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(22).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(23).png'))
-        self.sprites.append(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(24).png'))
+        size = (32, 32)
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(1).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(2).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(3).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(4).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(5).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(6).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(7).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(8).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(9).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(10).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(11).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(12).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(13).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(14).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(15).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(16).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(17).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(18).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(19).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(20).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(21).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(22).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(23).png'), size))
+        self.sprites.append(pg.transform.scale(pg.image.load('art_assets/pet-rock/rolling/rock-pet-animation-frames(24).png'), size))
 
         self.current_sprite = 0
         self.image = self.sprites[self.current_sprite]
 
-        #self.image = pg.image.load("art_assets/pet-rock/rock-pet-animation-stand.png").convert_alpha()
+        self.image = pg.image.load("art_assets/pet-rock/rock-pet-animation-stand.png").convert_alpha()
         self.image = pg.transform.rotozoom(self.image, 0, ((tile_size/2)/14))
         self.rect = self.image.get_rect(topleft = pos)
 
@@ -49,33 +50,32 @@ class Player(pg.sprite.Sprite):
         self.on_ceiling = False
         self.on_left = False
         self.on_right = False
+        self.is_animating = False
 
         #status of player for animation
-        self.status = 'art_assets/pet-rock/rock-pet-animation-stand.png'
 
-    #def import_character_assets(self):
-        #character_path = 'art_assets/pet-rock/rolling'
-        #self.animations = {'rolling':[]}
-        #for animation in self.animations.keys():
-            #full_path = character_path + animation
-            #self.animations[animation] = import_folder(full_path)
+        self.on_right = False
+        self.on_left = False
+        self.on_ceiling = False
+        self.on_floor = False
+        self.facing_right = True
 
-    #def get_status(self):
-        #move these later
-        #self.on_right = False
-        #self.on_left = False
-        #self.on_ceiling = False
-        #self.on_ground = False
-        #self.facing_right = True
-        #if self.direction.x != 0:
-           #self.status = 'rolling'
+        
+    def get_status(self):
+        if self.direction.y < 0:
+            self.status = self.animate()
+        elif self.direction.y > 1:
+            self.status = self.animate()
+        else:
+            if self.direction.x != 0:
+                self.status = self.animate()
+            else:
+                self.status = 'art_assets/pet-rock/rock-pet-animation-stand-32-32.png'
+        
 
-    #def animate(self):
-        #animation = self.animations('rolling')
-        #self.frame_index = self.animation_speed
-        #if self.frame_index >= len(animation):
-           #self.frame_index = 0
-        #self.image = animation[int(self.frame_index)]
+
+    def animate(self):
+        self.is_animating = True
 
     def key_input(self):
         keys = pg.key.get_pressed()
@@ -97,11 +97,16 @@ class Player(pg.sprite.Sprite):
 
     def update(self):
         self.key_input()
-        self.current_sprite += 1
+        if self.direction.x and self.direction.y != 0:
+           self.is_animating == True
+           self.current_sprite += 0.8
 
-        if self.current_sprite >= len(self.sprites):
-            self.current_sprite = 0
+           if self.current_sprite >= len(self.sprites):
+              self.current_sprite = 0
+              self.is_animating = False
+        else:
+            self.is_animating = False
+            self.status = 'art_assets/pet-rock/rock-pet-animation-stand-32-32.png'
 
-        self.image = self.sprites[self.current_sprite]
-        #self.animate()
-        #self.get_status()
+            self.image = self.sprites[int(self.current_sprite)]
+        self.animate()
